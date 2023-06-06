ea3164e78cdc758e03900bd71f85d74817727f0a
김명재 Myeongjae Kim <dev@myeongjae.kim>
2023-06-06 22:49:42 +0900

    feat: update kong.yml when executing install-env.sh

--- docker/install-env.sh
+++ docker/install-env.sh
@@ -6,8 +6,18 @@ ENVS=$(aws ssm get-parameters-by-path \
     --path "/$PATH_PREFIX/" \
     --with-decryption \
     --query "Parameters[*].[Name,Value]" \
-    --output text)
+    --output text | sed "s/^\/$PATH_PREFIX\///g" | sed "s/\t/=/g")
 
-echo "$ENVS" | sed "s/^\/$PATH_PREFIX\///g" | sed "s/\t/=/g" > .env
 
+# prepare .env
+echo "$ENVS" > .env
 cat .env.example | sed -e '1,10d' >> .env
+
+# prepare kong.yml
+ANON_KEY=$(echo "$ENVS" | grep 'ANON_KEY' | sed 's/ANON_KEY=//g')
+SERVICE_ROLE_KEY=$(echo "$ENVS" | grep 'SERVICE_ROLE_KEY' | sed 's/SERVICE_ROLE_KEY=//g')
+
+cp ./volumes/api/kong.yml.example ./volumes/api/kong.yml
+
+yq -i ".consumers[0].keyauth_credentials[0].key = \"$ANON_KEY\"" ./volumes/api/kong.yml
+yq -i ".consumers[1].keyauth_credentials[0].key = \"$SERVICE_ROLE_KEY\"" ./volumes/api/kong.yml
